642420683a469608a95b734548fddd568cf95c5b
nshkrdotcom <ZeroTrust@NSHkr.com>
2025-01-06 18:35:25 -1000

    fb

--- s.sh
+++ s.sh
@@ -2,7 +2,7 @@
 chmod +x r.sh
 ./r.sh
 mix deps.get
-mix test test/unit/provider_layer/providers/gemini/provider_test.exs --include live_call  2>&1 | tee >(xclip -selection clipboard)
+mix test #test/unit/provider_layer/providers/gemini/provider_test.exs --include live_call  2>&1 | tee >(xclip -selection clipboard)
 git add .
 git commit --m 'fb'
 git push
