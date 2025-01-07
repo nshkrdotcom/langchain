30a7b72c75170cbbfc7651c06b8b0664820f0706
nshkrdotcom <ZeroTrust@NSHkr.com>
2025-01-06 16:32:39 -1000

    fb

--- lib/provider_layer/providers/gemini/provider.ex
+++ lib/provider_layer/providers/gemini/provider.ex
@@ -1,17 +1,8 @@
-     {:ok, response} -> 
-       text = get_in(response, ["candidates", Access.at(0), "content", "parts", Access.at(0), "text"])
-       {:ok, text}
-     {:ok, response} -> 
-       text = get_in(response, ["candidates", Access.at(0), "content", "parts", Access.at(0), "text"])
-       {:ok, text}
-     {:ok, response} -> 
-       text = get_in(response, ["candidates", Access.at(0), "content", "parts", Access.at(0), "text"])
-       {:ok, text}
- def generate_content(prompt, opts \\ []) do
-   case GenerativeModel.generate_content(prompt, opts) do
-     {:ok, response} -> 
-       text = get_in(response, ["candidates", Access.at(0), "content", "parts", Access.at(0), "text"])
-       {:ok, text}
+def generate_content(prompt, opts \\ []) do
+  case GenerativeModel.generate_content(prompt, opts) do
+    {:ok, response} -> 
+      text = get_in(response, ["candidates", Access.at(0), "content", "parts", Access.at(0), "text"])
+      {:ok, text}
      error -> error
    end
- end
+end
