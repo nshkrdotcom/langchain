d8d6d687e9146300dfdc45c4124d220250b8500b
nshkrdotcom <ZeroTrust@NSHkr.com>
2025-01-06 18:39:28 -1000

    fb

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,11 +1,18 @@
 # Get verbosity level from environment
 #verbosity = System.get_env("TEST_VERBOSE", "false")
 
+# Get verbosity level from environment
+verbosity = System.get_env("TEST_VERBOSE", "false")
+
 ExUnit.configure(
   formatters: [ExUnit.CLIFormatter],
   trace: true,
-  #trace: verbosity == "true",
+  capture_log: verbosity != "true",
   exclude: [:skip, :live_call]
 )
+
+if verbosity == "true" do
+  Logger.configure(level: :debug)
+end
 
 ExUnit.start()
