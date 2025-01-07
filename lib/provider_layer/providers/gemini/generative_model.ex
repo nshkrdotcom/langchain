f409308f4031157561f3bace082753b6fe6b3cd5
nshkrdotcom <ZeroTrust@NSHkr.com>
2025-01-06 15:50:28 -1000

    fb

--- lib/provider_layer/providers/gemini/generative_model.ex
+++ lib/provider_layer/providers/gemini/generative_model.ex
@@ -11,7 +11,12 @@ defmodule LangChain.Google.GenerativeModel do
       LangChain.Middleware.PersistenceMiddleware
     ]
 
-  def generate_content(prompt, opts \\ []) do
+  # Function header with default value
+  @spec generate_content(String.t(), keyword()) :: {:ok, map()} | {:error, term()}
+  def generate_content(prompt, opts \\ [])
+
+  # Actual implementation
+  def generate_content(prompt, opts) do
     with {:ok, response} <- Client.generate_content(prompt, opts) do
       case Keyword.get(opts, :response_mime_type) do
         "application/json" -> {:ok, response}
--- r.sh
+++ r.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 
-# Fix generative_model.ex issues
+# Fix generative_model.ex
 cat > lib/provider_layer/providers/gemini/generative_model.ex << 'EOL'
 defmodule LangChain.Google.GenerativeModel do
   @moduledoc """
@@ -16,7 +16,12 @@ defmodule LangChain.Google.GenerativeModel do
       LangChain.Middleware.PersistenceMiddleware
     ]
 
-  def generate_content(prompt, opts \\ []) do
+  # Function header with default value
+  @spec generate_content(String.t(), keyword()) :: {:ok, map()} | {:error, term()}
+  def generate_content(prompt, opts \\ [])
+
+  # Actual implementation
+  def generate_content(prompt, opts) do
     with {:ok, response} <- Client.generate_content(prompt, opts) do
       case Keyword.get(opts, :response_mime_type) do
         "application/json" -> {:ok, response}
@@ -27,54 +32,3 @@ defmodule LangChain.Google.GenerativeModel do
 end
 EOL
 
-# Create behavior.ex for middleware
-cat > lib/langchain/middleware/behaviour.ex << 'EOL'
-defmodule LangChain.Middleware.Behavior do
-  @moduledoc """
-  Defines the behavior for middleware modules.
-  """
-  
-  @callback handle_request(request :: term(), opts :: keyword()) ::
-              {:ok, term()} | {:error, term()}
-              
-  @callback handle_response(response :: term(), opts :: keyword()) ::
-              {:ok, term()} | {:error, term()}
-end
-EOL
-
-# Fix persistence config
-cat > lib/langchain/persistence/config.ex << 'EOL'
-defmodule LangChain.Persistence.Config do
-  @moduledoc """
-  Configuration for persistence layer
-  """
-  
-  def get_persistence_backend do
-    Application.get_env(:langchain, :persistence_backend, LangChain.Persistence.Memory)
-  end
-  
-  def adapter do
-    Application.get_env(:langchain, :persistence_adapter, LangChain.Persistence.PostgresAdapter)
-  end
-end
-EOL
-
-# Fix config.ex
-cat > lib/langchain/config.ex << 'EOL'
-defmodule LangChain.Config do
-  @moduledoc """
-  Configuration for LangChain
-  """
-  
-  def get_provider(opts \\ []) do
-    Keyword.get(opts, :provider, Application.get_env(:langchain, :default_provider))
-  end
-  
-  def get_persistence_backend do
-    Application.get_env(:langchain, :persistence_backend, LangChain.Persistence.Memory)
-  end
-end
-EOL
-
-# Run mix compile to verify fixes
-mix compile
