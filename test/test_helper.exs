3e47084864c7ad965ce6393ae3d82a7e0bf68979
nshkrdotcom <ZeroTrust@NSHkr.com>
2025-01-06 17:47:26 -1000

    fb

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,7 +4,7 @@ verbosity = System.get_env("TEST_VERBOSE", "false")
 ExUnit.configure(
   formatters: [ExUnit.CLIFormatter],
   trace: verbosity == "true",
-  exclude: [:skip]
+  exclude: [:skip, :live_call]
 )
 
 ExUnit.start()
