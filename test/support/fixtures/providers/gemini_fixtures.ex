5666d7b4382d982d432e0dbf64c70e7be4cebc7b
nshkrdotcom <ZeroTrust@NSHkr.com>
2025-01-06 17:16:14 -1000

    fb

--- test/support/fixtures/providers/gemini_fixtures.ex
+++ test/support/fixtures/providers/gemini_fixtures.ex
@@ -22,7 +22,7 @@ defmodule LangChain.Test.Fixtures.Providers.GeminiFixtures do
           "content" => %{
             "parts" => [
               %{
-                "text" => ""```json\n{\"programming_languages\":[{\"name\":\"Python\",\"description\":\"A high-level, interpreted language\",\"features\":[\"Interpreted\",\"Easy to read\"]},{\"name\":\"Java\",\"description\":\"Object-oriented language\",\"features\":[\"Compiled\",\"Portable\"]}]}\n```"
+                "text" => "```json\n{\"programming_languages\":[{\"name\":\"Python\",\"description\":\"A high-level, interpreted language\",\"features\":[\"Interpreted\",\"Easy to read\"]},{\"name\":\"Java\",\"description\":\"Object-oriented language\",\"features\":[\"Compiled\",\"Portable\"]}]}\n```"
               }
             ]
           }
