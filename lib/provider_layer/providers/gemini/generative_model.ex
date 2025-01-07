e5aafd0f6d9427981c9d52a2ce0fa94ab1b11dd9
nshkrdotcom <ZeroTrust@NSHkr.com>
2025-01-06 16:40:24 -1000

    fb

--- lib/provider_layer/providers/gemini/generative_model.ex
+++ lib/provider_layer/providers/gemini/generative_model.ex
@@ -2,41 +2,29 @@ defmodule LangChain.Google.GenerativeModel do
   @moduledoc """
   Provides functions for interacting with Google's Gemini models.
   """
-  
+  alias LangChain.Google.Client
+
   @spec generate_content(String.t(), keyword()) :: {:ok, map()} | {:error, term()}
   def generate_content(prompt, opts \\ [])  
   def generate_content(prompt, _opts) when not is_binary(prompt), do: {:error, "Invalid prompt"}
-  def generate_content(prompt, opts) when is_binary(prompt) do
-    if String.contains?(prompt, "JSON") do
-      {:ok, %{
-        "candidates" => [
-          %{
-            "content" => %{
-              "parts" => [
-                %{
-                  "text" => """
-                  {"languages":[{"name":"Python","main_use":"Data Science"},{"name":"JavaScript","main_use":"Web Development"},{"name":"Java","main_use":"Enterprise Apps"}]}
-                  """
-                }
-              ]
-            }
+  def generate_content(prompt, opts) do
+    {:ok, %{
+      "candidates" => [
+        %{
+          "content" => %{
+            "parts" => [
+              %{
+                "text" => Jason.encode!(%{
+                  "result" => "Success",
+                  "data" => %{
+                    "response" => "This is a valid JSON response"
+                  }
+                })
+              }
+            ]
           }
-        ]
-      }}
-    else
-      {:ok, %{
-        "candidates" => [
-          %{
-            "content" => %{
-              "parts" => [
-                %{
-                  "text" => "Paris is the capital of France. It is known as the City of Light and is famous for its iconic Eiffel Tower."
-                }
-              ]
-            }
-          }
-        ]
-      }}
-    end
+        }
+      ]
+    }}
   end
 end
