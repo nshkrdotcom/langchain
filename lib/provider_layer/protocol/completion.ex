e369f7a3bc5359c1c14bb059509d412a000c4bea
nshkrdotcom <ZeroTrust@NSHkr.com>
2025-01-06 20:59:52 -1000

    fb

--- lib/provider_layer/protocol/completion.ex
+++ lib/provider_layer/protocol/completion.ex
@@ -1,8 +1,1 @@
-defmodule LangChain.Capabilities.Completion do
-  alias LangChain.Config
-
-  def complete(prompt, opts \\ []) do
-    provider = Config.get_provider(opts)
-    provider.generate_content(prompt, opts)
-  end
-end
+# langchain/lib/langchain/protocol/completion.ex
